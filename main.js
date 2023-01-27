b844556856914b42508cf8e96f2f9e25eeebb480
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-01-27 01:14:07 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+export const getElementById = (parameter) => { return document.getElementById(parameter); };
+export const addString = (parameter) => { return parameter; };
+export const typechart = (parameter) => { return addString(parameter); }; // "bar"
+export const label = (parameter) => { return addString(parameter); }; // "# of Votes"
