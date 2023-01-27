38f230dd0329e0091da00e539623f446e45c3fcd
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-01-27 01:54:17 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 import { labels, data, borderwidth, beginAtZero} from  "./constants.js";
 import { getElementById, typechart, label} from  "./module.js";
-export const ObjectHere = Object.freeze({ id: getElementById('myChart'), typechart: typechart('bar'),  labels: labels, data: data, label: label('# of Votes'), borderwidth:borderwidth, beginAtZero:beginAtZero})
+export const ObjectHere = Object.freeze({ id: getElementById('myChart'), typechart: typechart('bar'),  labels: labels, data: data, label: label('# of Votes'), borderwidth:borderwidth, beginAtZero:beginAtZero});
