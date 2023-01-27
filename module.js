70ce2e2fad3a908abe86548c69ded13e2226e2da
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-01-27 00:26:19 -0300

    Create module.js

---
+++ module.js
@@ -0,0 +1,7 @@
+export const ctx = document.getElementById("myChart");
+export const typeChart = "bar";
+export const array_labels = ["Reds", "Blue", "Yellow", "Green", "Purple", "Orange"];
+export const array_data = [12, 19, 3, 5, 2, 3];
+export const string_label = "# of Votes";
+export const border_width = 1;
+export const begin_at_zero = true;
