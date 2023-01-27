79e9444ec0eec602206ed8e67913c5532a53899b
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-01-27 00:56:01 -0300

    Update module.js

--- module.js
+++ module.js
@@ -5,3 +5,38 @@ export const array_data = [12, 19, 3, 5, 2, 3];
 export const string_label = "# of Votes";
 export const border_width = 1;
 export const begin_at_zero = true;
+export class BarChart {
+  constructor(id, typechart, labels, data, borderwidth, beginAtZero) {
+    this.id = id;
+    this.typechart= typechart;
+    this.labels= labels;
+    this.data= data;
+    this.label= label;
+    this.borderwidth= borderwidth;
+    this.beginAtZero= beginAtZero;
+  }
+  generateGraph() {
+    return new Chart(this.id, {
+        type: this.typechart,
+        data: {
+          labels: this.labels,
+          datasets: [
+            {
+              label: this.label,
+              data: this.data,
+              borderWidth: this.borderwidth,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: this.beginAtZero,
+            },
+          },
+        },
+      }); 
+    }
+  }
+
+export {BarChart};
