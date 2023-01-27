2d8035b6ba2bf3b09516d9c3fe1359c3abdfccfd
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-01-27 01:13:52 -0300

    Update module.js

--- module.js
+++ module.js
@@ -1,8 +1,4 @@
-export const getElementById = (parameter) => { return document.getElementById(parameter); };
-export const addString = (parameter) => { return parameter; };
-export const typechart = (parameter) => { return addString(parameter); }; // "bar"
 export const labels = ["Reds", "Blue", "Yellow", "Green", "Purple", "Orange"];
 export const data = [12, 19, 3, 5, 2, 3];
-export const label = (parameter) => { return addString(parameter); }; // "# of Votes"
 export const borderwidth = 1;
 export const beginAtZero = true;
